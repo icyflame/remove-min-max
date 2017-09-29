6078d0acae86291c749bb115bcd9419560498779
Jan-Ka <jan-ka@users.noreply.github.com>
2017-09-29 17:26:05 +0200

    added more meaningful tests this check in contains a breaking test

--- test.js
+++ test.js
@@ -3,8 +3,43 @@
 var assert = require('assert');
 var removeMinMax = require('./');
 
-it('should work on arrays', function () {
-  assert.deepEqual(removeMinMax([1, 2, 3, 5, 6, 9, 4, 7, 8]), [2, 3, 4, 5, 6, 7, 8]);
-  assert.deepEqual(removeMinMax([1, 2]), []);
-  assert.deepEqual(removeMinMax([0.001, 0.1, 1.0, 200, 5000, 2.5, 2.95]), [0.1, 1.0, 2.5, 2.95, 200]);
+it('returns undefined on non-array values', function () {
+  var actual = removeMinMax('string');
+
+  assert.equal(actual); // mocha complains about initializing expected to undefined
+});
+
+it('returns empty array when length of array is sufficient', function () {
+  var actual = removeMinMax([1, 2]);
+  var expected = [];
+
+  assert.deepEqual(actual, expected);
+});
+
+it('accepts number (integer) values', function () {
+  var actual = removeMinMax([1, 2, 3, 4, 5000, 5, 6]);
+  var expected = [2, 3, 4, 5, 6];
+
+  assert.deepEqual(actual, expected);
+});
+
+it('accepts number (float) values', function () {
+  var actual = removeMinMax([0.001, 0.1, 1.0, 2.5, 2.95]);
+  var expected = [0.1, 1.0, 2.5];
+
+  assert.deepEqual(actual, expected);
+});
+
+it('accepts mixed number values (integer, float)', function () {
+  var actual = removeMinMax([0.001, 0.1, 1.0, 200, 5000, 2.5, 2.95]);
+  var expected = [0.1, 1.0, 2.5, 2.95, 200];
+
+  assert.deepEqual(actual, expected);
+});
+
+it('keeps references intact', function () {
+  var expected = [1, 2, 3, 4, 5000, 5, 6];
+  var actual = removeMinMax(expected);
+
+  assert.notDeepEqual(actual, expected);
 });
